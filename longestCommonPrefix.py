3fb6195c9804a784f1122c42acda9034fe7a44dc
arjun-baidya <arjun.chandra.baidya@gmail.com>
2021-12-26 16:18:53 +0600

    solved

---
+++ longestCommonPrefix.py
@@ -0,0 +1,13 @@
+
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+    for i in range(len(strs[0])):
+        for string in strs[1:]:
+            if i == len(string) or string[i] != strs[0][i]:
+                return strs[0][:i]
+    return strs[0]
+
+
+a =longestCommonPrefix(["flower","flow","flowight"])
+print(a)
